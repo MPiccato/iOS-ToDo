ce46053bbf18b922e6f43d68675cbfde0cb5a1e2
mpiccato <mpiccato@me.com>
2023-01-02 08:02:48 -0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Martin Piccato on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
